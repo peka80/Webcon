d36bfa53e4888b7719aa344e00f0fb468a059aae
peka80 <petar.dragovic@gmail.com>
2021-03-19 13:25:50 +0100

    socials slider dopuna

--- ION_Radionica/main.js
+++ ION_Radionica/main.js
@@ -2,6 +2,9 @@ $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 50,
   nav: true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplaySpeed: 1000,
   responsive: {
     0: {
       items: 1,
@@ -10,7 +13,7 @@ $('.owl-carousel').owlCarousel({
       items: 1,
     },
     1000: {
-      items: 2,
+      items: 1,
     },
   },
 });
